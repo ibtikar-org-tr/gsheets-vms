6992481b82dbfc349e4229aeef80741b6cce3d42
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 01:36:10 +0300

    Add sheet retrieval and update functions in sheet_service.py

--- app/services/sheet_service.py
+++ app/services/sheet_service.py
@@ -7,3 +7,16 @@ def get_all_sheets():
 def create_new_sheet(sheet: sheet_model.Sheet):
     sheets_db.db_list.append(sheet)
     return sheet
+
+def get_sheet_by_id(sheet_id: int):
+    for sheet in sheets_db.db_list:
+        if sheet.id == sheet_id:
+            return sheet
+    return None
+
+def update_sheet_by_id(sheet_id: int, sheet: sheet_model.Sheet):
+    for i, s in enumerate(sheets_db.db_list):
+        if s.id == sheet_id:
+            sheets_db.db_list[i] = sheet
+            return sheet
+    return None
