9cb506f4229761645ec9bb3fb09c28af8f6f441c
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 17:46:41 +0300

    Add endpoints to retrieve all tasks and check all sheets

--- app/routers/task_routers.py
+++ app/routers/task_routers.py
@@ -3,6 +3,15 @@ from app.handlers import task_handlers
 
 router = APIRouter()
 
+@router.get("/tasks")
+async def get_all_tasks():
+    return await task_handlers.get_all_tasks()
+
 @router.post("/check_tasks")
 async def check_tasks_from_sheet(id: str):
     return await task_handlers.check_tasks_from_sheet(id)
+
+@router.get("/check_all_sheets")
+async def check_all_sheets():
+    return await task_handlers.check_all_sheets()
+
