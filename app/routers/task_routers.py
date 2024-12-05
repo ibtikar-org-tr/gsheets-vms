680391e434fb5007b93dc70cdab445244cf694c3
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 19:43:25 +0300

    Change check_all_sheets endpoint from GET to POST for task checking

--- app/routers/task_routers.py
+++ app/routers/task_routers.py
@@ -12,7 +12,8 @@ async def check_tasks_from_sheet(id: str):
     await task_handlers.check_tasks_from_sheet(id)
     return {"message": "Tasks checked successfully"}
 
-@router.get("/check_all_sheets")
+
+@router.post("/check_all_sheets")
 async def check_all_sheets():
     await task_handlers.check_all_sheets()
     return {"message": "all sheets' Tasks checked successfully"}
