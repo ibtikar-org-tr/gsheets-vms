2c6f0af1312b6c91c89152ba8d99b58f6a5bc866
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-06 01:32:41 +0300

    Improve error handling in task sheet parsing by updating exception messages for clarity

--- app/handlers/task_handlers.py
+++ app/handlers/task_handlers.py
@@ -16,7 +16,7 @@ async def check_tasks_from_sheet(sheet_id: str):
     except Exception as e:
         logger.error(f"Error parsing sheet id {sheet_id}: {e}")
         logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
-        raise HTTPException(status_code=400, detail="Invalid sheet id")
+        raise HTTPException(status_code=400, detail="error parsing sheet id")
     
 async def check_all_sheets():
     try:
@@ -24,4 +24,4 @@ async def check_all_sheets():
     except Exception as e:
         logger.error(f"Error parsing sheet ids: {e}")
         logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
-        raise HTTPException(status_code=400, detail="Invalid sheet id")
+        raise HTTPException(status_code=400, detail="error parsing sheet ids")
