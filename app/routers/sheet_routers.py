030e7b37c1c1bc8423fe18579eeba83cb39958c3
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 11:40:37 +0300

    Add sheet_handlers for improved sheet management and refactor routers to use handlers

--- app/routers/sheet_routers.py
+++ app/routers/sheet_routers.py
@@ -1,43 +1,21 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.models import sheet_model
-from app.services import sheet_service
-import logging
+from app.handlers import sheet_handlers
 
 router = APIRouter()
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 @router.get("/sheets/", response_model=list[sheet_model.Sheet])
 def get_sheets():
-    return sheet_service.get_all_sheets()
+    return sheet_handlers.get_sheets()
 
 @router.post("/sheets/", response_model=sheet_model.Sheet)
 def create_sheet(sheet: sheet_model.Sheet):
-    return sheet_service.create_new_sheet(sheet)
+    return sheet_handlers.create_sheet(sheet)
 
 @router.get("/sheets/{sheet_id}", response_model=sheet_model.Sheet)
 def get_sheet_by_id(sheet_id: int):
-    print(f"Fetching sheet with id {sheet_id}")
-    try:
-        sheet = sheet_service.get_sheet_by_id(sheet_id)
-        if sheet is None:
-            logger.warning(f"Sheet with id {sheet_id} not found")
-            raise HTTPException(status_code=404, detail="Sheet not found")
-        return sheet
-    except Exception as e:
-        logger.error(f"Error retrieving sheet with id {sheet_id}: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+    return sheet_handlers.get_sheet_by_id(sheet_id)
 
 @router.put("/sheets/{sheet_id}", response_model=sheet_model.Sheet)
 def update_sheet(sheet_id: int, sheet: sheet_model.Sheet):
-    try:
-        updated_sheet = sheet_service.update_sheet_by_id(sheet_id, sheet)
-        if updated_sheet is None:
-            logger.warning(f"Sheet with id {sheet_id} not found")
-            raise HTTPException(status_code=404, detail="Sheet not found")
-        return updated_sheet
-    except Exception as e:
-        logger.error(f"Error updating sheet with id {sheet_id}: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+    return sheet_handlers.update_sheet(sheet_id, sheet)
