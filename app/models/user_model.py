0043940a231806648973f2090d79dde9009f3bd2
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-06 01:02:21 +0300

    Add created_at and last_login fields to User model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class User(BaseModel):
     id: int
     username: str
     email: str
     password: str
+    created_at: datetime
+    last_login: datetime
+
