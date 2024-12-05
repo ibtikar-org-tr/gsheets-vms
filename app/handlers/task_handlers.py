5bc2c49a42357fbd2d15d3f19e10b28ed7055d65
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 17:46:32 +0300

    Refactor task_handlers to replace gsheet_service with task_service and update task handling functions

--- app/handlers/task_handlers.py
+++ app/handlers/task_handlers.py
@@ -1,15 +1,24 @@
 from fastapi import HTTPException
 from app.models import sheet_model
-from app.services import gsheet_service
+from app.services import task_service
 import logging
 
 logger = logging.getLogger(__name__)
 
-def check_tasks_from_sheet(sheet_link: str):
+def get_all_tasks():
+    return task_service.get_all_tasks()
+
+def check_tasks_from_sheet(sheet_id: str):
     try:
-        sheet_id = gsheet_service.check_tasks_from_sheet(sheet_link)
-        return sheet_id
+        task_service.check_tasks_from_sheet(sheet_id)
     except Exception as e:
-        logger.error(f"Error parsing sheet link {sheet_link}: {e}")
-        raise HTTPException(status_code=400, detail="Invalid sheet link")
+        logger.error(f"Error parsing sheet link {sheet_id}: {e}")
+        raise HTTPException(status_code=400, detail="Invalid sheet id")
     
+
+def check_all_sheets():
+    try:
+        task_service.check_all_sheets()
+    except Exception as e:
+        logger.error(f"Error parsing sheet ids: {e}")
+        raise HTTPException(status_code=400, detail="Invalid sheet id")
