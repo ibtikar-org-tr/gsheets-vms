ba160cea2e8f9c1f3f5f4c1c93edf3df640fccb3
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 11:35:24 +0300

    Refactor sheet_service to use new database structure with db.sheet_list

--- app/services/sheet_service.py
+++ app/services/sheet_service.py
@@ -1,22 +1,22 @@
-from app.db import sheets_db
+from app.db import db
 from app.models import sheet_model
 
 def get_all_sheets():
-    return sheets_db.db_list
+    return db.sheet_list
 
 def create_new_sheet(sheet: sheet_model.Sheet):
-    sheets_db.db_list.append(sheet)
+    db.sheet_list.append(sheet)
     return sheet
 
 def get_sheet_by_id(sheet_id: int):
-    for sheet in sheets_db.db_list:
+    for sheet in db.sheet_list:
         if sheet.id == sheet_id:
             return sheet
     return None
 
 def update_sheet_by_id(sheet_id: int, sheet: sheet_model.Sheet):
-    for i, s in enumerate(sheets_db.db_list):
+    for i, s in enumerate(db.sheet_list):
         if s.id == sheet_id:
-            sheets_db.db_list[i] = sheet
+            db.sheet_list[i] = sheet
             return sheet
     return None
