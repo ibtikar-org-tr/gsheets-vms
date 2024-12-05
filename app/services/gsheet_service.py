40d74e7fd40ed1ea44842e1cc35b5a112bf9c771
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-05 17:24:15 +0300

    Add gsheet_service for Google Sheets integration and task management

--- app/services/gsheet_service.py
+++ app/services/gsheet_service.py
@@ -1,13 +1,17 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os
+import time
+from app.services import task_service
+from app.models import task_model
 
 # Define the scope for Google Sheets and Drive API
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", 
           "https://www.googleapis.com/auth/drive"]
 
-def get_gsheet(api_json, sheet_id):
+def get_gsheet(sheet_id):
     # Path to your service account key file
-    SERVICE_ACCOUNT_FILE = api_json
+    SERVICE_ACCOUNT_FILE = os.getenv("GOOGLE_API_JSON_PATH")
 
     # Authenticate with the service account
     credentials = Credentials.from_service_account_file(
@@ -38,11 +42,16 @@ def get_specific_contact(contacts_page, contact_name):
 # search for a contact details by name
     for contact in contacts_page:
         if contact['name1'] == contact_name:
-            print("name1: ", contact['name1'])
-            print("phone: ", contact['phone'])
-            print("mail: ", contact['mail'])
-            print("sex: ", contact['sex'])
-            print("mission-ar: ", contact['mission-ar'])
-            print("total points: ", contact['total points'])
-            print("rank: ", contact['rank'])
+            # print("name1: ", contact['name1'])
+            # print("number: ", contact['number'])
+            # print("name2: ", contact['name2'])
+            # print("phone: ", contact['phone'])
+            # print("mail: ", contact['mail'])
+            # print("sex: ", contact['sex'])
+            # print("mission-ar: ", contact['mission-ar'])
+            # print("total points: ", contact['total points'])
+            # print("rank: ", contact['rank'])
+            # print("global Rank: ", contact['global Rank'])
             return contact
+
+
