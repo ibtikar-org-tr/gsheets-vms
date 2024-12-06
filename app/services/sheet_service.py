7f31ddd4f0ad335655d065b30690d419a4fa79e1
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-07 00:11:25 +0300

    Refactor sheet service functions to use string IDs and add delete_sheet_by_id function

--- app/services/sheet_service.py
+++ app/services/sheet_service.py
@@ -4,24 +4,26 @@ from app.db import db_connection
 from sqlmodel import select
 
 def get_all_sheets():
+    print("point3: sheet_service.get_all_sheets")
     with db_connection.get_session() as session:
         sheets = session.exec(select(sheet_model.Sheet)).all()
     return sheets
 
-def create_new_sheet(sheet: sheet_model.Sheet):
+def create_new_sheet(sheetID: str):
+    sheet = sheet_model.Sheet(sheetID=sheetID)
     with db_connection.get_session() as session:
         session.add(sheet)
         session.commit()
         session.refresh(sheet)
     return sheet
 
-def get_sheet_by_id(sheet_id: int):
+def get_sheet_by_id(sheet_id: str):
     with db_connection.get_session() as session:
         sheet = session.get(sheet_model.Sheet, sheet_id)
     return sheet
 
 
-def update_sheet_by_id(sheet_id: int, sheet: sheet_model.Sheet):
+def update_sheet_by_id(sheet_id: str, sheet: sheet_model.Sheet):
     with db_connection.get_session() as session:
         existing_sheet = session.get(sheet_model.Sheet, sheet_id)
         if existing_sheet:
@@ -31,3 +33,11 @@ def update_sheet_by_id(sheet_id: int, sheet: sheet_model.Sheet):
             session.commit()
             session.refresh(existing_sheet)
         return existing_sheet
+
+def delete_sheet_by_id(sheet_id: str):
+    with db_connection.get_session() as session:
+        sheet = session.get(sheet_model.Sheet, sheet_id)
+        if sheet:
+            session.delete(sheet)
+            session.commit()
+        return sheet
