3899869fb6e4cc1556d9f07130cd6d9ea692ed43
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-06 21:57:24 +0300

    Fix error messages in task_handlers.py for better clarity

--- app/handlers/task_handlers.py
+++ app/handlers/task_handlers.py
@@ -1,5 +1,4 @@
 from fastapi import HTTPException
-from app.models import sheet_model
 from app.services import task_service
 import logging
 
@@ -16,12 +15,12 @@ async def check_tasks_from_sheet(sheet_id: str):
     except Exception as e:
         logger.error(f"Error parsing sheet id {sheet_id}: {e}")
         logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
-        raise HTTPException(status_code=400, detail="error parsing sheet id")
-    
+        raise HTTPException(status_code=400, detail="Error parsing sheet id")
+
 async def check_all_sheets():
     try:
         task_service.check_all_sheets()
     except Exception as e:
         logger.error(f"Error parsing sheet ids: {e}")
         logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
-        raise HTTPException(status_code=400, detail="error parsing sheet ids")
+        raise HTTPException(status_code=400, detail="Error parsing sheet ids")
