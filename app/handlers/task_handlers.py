af8575346eeb9539fca727cf4e4fd5cf9325d6b8
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-07 01:01:07 +0300

    Add task scheduler function with error handling and logging

--- app/handlers/task_handlers.py
+++ app/handlers/task_handlers.py
@@ -25,3 +25,11 @@ async def check_all_sheets():
         logger.error(f"Error parsing sheet ids: {e}")
         logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
         raise HTTPException(status_code=400, detail="Error parsing sheet ids")
+    
+async def run_task_15min_schedualer():
+    try:
+        task_service.run_task_15min_schedualer()
+    except Exception as e:
+        logger.error(f"Error running task scheduler: {e}")
+        logger.error(f"Error occurred in file {e.__traceback__.tb_frame.f_code.co_filename}, function {e.__traceback__.tb_frame.f_code.co_name}, line {e.__traceback__.tb_lineno}")
+        raise HTTPException(status_code=400, detail="Error running task scheduler")
