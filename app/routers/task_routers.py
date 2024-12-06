5d1aa44d896e79fe1606c8b6409527370d3647b0
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-07 01:01:11 +0300

    Add endpoint to run task scheduler every 15 minutes

--- app/routers/task_routers.py
+++ app/routers/task_routers.py
@@ -12,8 +12,12 @@ async def check_tasks_from_sheet(id: str):
     await task_handlers.check_tasks_from_sheet(id)
     return {"message": "Tasks checked successfully"}
 
-
 @router.post("/check_all_sheets")
 async def check_all_sheets():
     await task_handlers.check_all_sheets()
     return {"message": "all sheets' Tasks checked successfully"}
+
+@router.post("/run_task_15min_schedualer")
+async def run_task_15min_schedualer():
+    await task_handlers.run_task_15min_schedualer()
+    return {"message": "Background task started to check all sheets every 15 minutes"}
