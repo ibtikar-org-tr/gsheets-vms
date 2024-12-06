11627632ffd92606c5374489a775edf40fb58877
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-06 21:58:30 +0300

    Refactor Task model to use UUID for primary key

--- app/models/task_model.py
+++ app/models/task_model.py
@@ -1,8 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
-
-
+import uuid
 
 class Task(SQLModel, table=True):
     id: Optional[str] = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
