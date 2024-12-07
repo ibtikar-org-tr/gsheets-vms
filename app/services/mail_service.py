c5a9c58631b185a456d1d0df0038fb9cd95b6811
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-07 21:41:48 +0300

    Enhance mail_service to send SMS using requests and restore email sending logic

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import requests
 
 def send_email(to, subject, message):
     SMTP_PORT = os.getenv('SMTP_PORT')
@@ -10,22 +11,27 @@ def send_email(to, subject, message):
     SMTP_HOST = os.getenv('SMTP_HOST')
 
     try:
-        # msg = MIMEMultipart()
-        # msg['From'] = SMTP_USER
-        # msg['To'] = to
-        # msg['Subject'] = subject
+        msg = MIMEMultipart()
+        msg['From'] = SMTP_USER
+        msg['To'] = to
+        msg['Subject'] = subject
 
-        # msg.attach(MIMEText(message, 'plain'))
+        msg.attach(MIMEText(message, 'plain'))
 
-        # server = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
-        # server.starttls()
-        # server.login(SMTP_USER, SMTP_PASS)
-        # server.send_message(msg)
-        # server.quit()
+        server = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASS)
+        server.send_message(msg)
+        server.quit()
 
         print(f"Email sent successfully to {to}")
     except Exception as e:
         print(f"Failed to send email: {e}")
 
 def send_sms(phone, message):
-    print("SMS sent successfully to ", phone)
+    try:
+        response = requests.post(f"http://127.0.0.1:3020/send?phone={phone}&message={message}")
+        response.raise_for_status()
+        print("SMS sent successfully to ", phone)
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send SMS: {e}")
