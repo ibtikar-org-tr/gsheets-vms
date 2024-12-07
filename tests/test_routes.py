33943b32a152ba627d29c22aac8d01e58dd29913
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-07 08:22:24 +0300

    Remove obsolete test file for sheet retrieval endpoint

--- tests/test_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_get_sheets():
-    response = client.get("/sheets/")
-    assert response.status_code == 200
-    assert response.json() == []
