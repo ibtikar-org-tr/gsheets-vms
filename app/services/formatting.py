2d470f961b12ff0384ef5a2a1610eecbf2ed9ef1
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-17 16:55:25 +0300

    Add additional date formats to strptime2 for improved parsing flexibility

--- app/services/formatting.py
+++ app/services/formatting.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # Date & Time
 def strptime2(date_string):
-    return strptime_format(date_string, ["%Y-%m-%d", "%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S.%f"])
+    return strptime_format(date_string, ["%Y-%m-%d", "%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S.%f", "%d/%m/%Y", "%d/%m/%Y %H:%M:%S", "%d/%m/%Y %H:%M:%S.%f"])
 
 def strptime_format(date_string, formats):
     for fmt in formats:
