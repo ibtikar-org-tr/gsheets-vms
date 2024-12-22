a05dfe1eca4b0f22d0dca90c90aa096f09304889
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-23 00:17:19 +0300

    Add function to check Google Drive folder permissions

--- app/services/gdrive_service.py
+++ app/services/gdrive_service.py
@@ -70,7 +70,39 @@ def remove_permission_from_gdrive_folder(folder_link: str, mail: str):
     
     return response
 
+def check_drive_folder_permission(folder_link: str):
+    SERVICE_ACCOUNT_FILE = os.getenv("GOOGLE_API_JSON_PATH")
+
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    # Authenticate with the service account
+    try:
+        credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES
+        )
+    except Exception as e:
+        print(f"Authentication failed (gsheet): {e}")
+        return None
+    
+    # Connect to Google Drive
+    drive_service = build('drive', 'v3', credentials=credentials)
+
+    try:
+        # Get the permission details of the folder
+        response = drive_service.permissions().list(
+            fileId=folder_link_to_id(folder_link),
+            fields='permissions(emailAddress,type,role)'
+        ).execute()
+        print(f"Folder permissions: {response}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+    return response
+
 def folder_link_to_id(folder_link: str) -> str:
     # Extract the folder ID from the link
     folder_id = folder_link.split('/')[-1].split('?')[0]
     return folder_id
+
+
