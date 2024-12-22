ca61f4f1488928375087827823afe9f1af8b0cf8
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-23 01:07:34 +0300

    Refactor email permission handling in drive_service to improve clarity and error management

--- app/services/drive_service.py
+++ app/services/drive_service.py
@@ -1,32 +1,47 @@
 from app.services import gdrive_service
 
 def check_list_of_mails(folder_link: str, page_mails: list):
-    mails_added = []
-    mails_deleted = []
+    will_be_added = []
+    will_be_deleted = []
     existing_mails = []
 
     exising_permissions = gdrive_service.check_drive_folder_permission(page_mails)
     for permission in exising_permissions['permissions']:
-        if permission['type'] == 'user' and not permission['role'] == 'owner':
+        if permission['type'] == 'user' and not permission['roleZ'] == 'owner':
             existing_mails.append(permission['emailAddress'])
 
     for mail in page_mails:
         if mail not in existing_mails:
             try:
-                gdrive_service.add_permission_to_gdrive_folder(folder_link, mail)
-                mails_added.append(mail)
+                # gdrive_service.add_permission_to_gdrive_folder(folder_link, mail)
+                will_be_added.append(mail)
             except Exception as e:
                 print(f"An error occurred when adding {mail}: {e}")
     
     for mail in existing_mails:
         if mail not in page_mails:
             try:
-                gdrive_service.remove_permission_from_gdrive_folder(folder_link, mail)
-                mails_deleted.append(mail)
+                # gdrive_service.remove_permission_from_gdrive_folder(folder_link, mail)
+                will_be_deleted.append(mail)
             except Exception as e:
                 print(f"An error occurred when deleting {mail}: {e}")
 
-    print(f"added: {mails_added}")
-    print(f"deleted: {mails_deleted}")
+    for mail in will_be_added:
+        try:
+            gdrive_service.add_permission_to_gdrive_folder(folder_link, mail)
+            print(f"added: {will_be_added}")
+        except Exception as e:
+            print(f"An error occurred when adding {mail}: {e}")
+
+    for mail in will_be_deleted:
+        try:
+            gdrive_service.remove_permission_from_gdrive_folder(folder_link, mail)
+            print(f"deleted: {will_be_deleted}")
+        except Exception as e:
+            print(f"An error occurred when deleting {mail}: {e}")
+
+
+    print(f"added: {will_be_added}")
+    print(f"deleted: {will_be_deleted}")
 
 
