1cd2df599766ed994cc80d4bd89c11522903b205
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-23 01:31:01 +0300

    Refactor permission handling in check_list_of_mails to use folder_link consistently and improve error handling

--- app/services/drive_service.py
+++ app/services/drive_service.py
@@ -5,9 +5,9 @@ def check_list_of_mails(folder_link: str, page_mails: list):
     will_be_deleted = []
     existing_mails = []
 
-    exising_permissions = gdrive_service.check_drive_folder_permission(page_mails)
+    exising_permissions = gdrive_service.check_drive_folder_permission(folder_link)
     for permission in exising_permissions['permissions']:
-        if permission['type'] == 'user' and not permission['roleZ'] == 'owner':
+        if permission['type'] == 'user' and not permission['role'] == 'owner':
             existing_mails.append(permission['emailAddress'])
 
     for mail in page_mails:
@@ -28,14 +28,14 @@ def check_list_of_mails(folder_link: str, page_mails: list):
 
     for mail in will_be_added:
         try:
-            gdrive_service.add_permission_to_gdrive_folder(folder_link, mail)
+            gdrive_service.add_permission_to_gdrive_folder(folder_link = folder_link, mail = mail)
             print(f"added: {will_be_added}")
         except Exception as e:
             print(f"An error occurred when adding {mail}: {e}")
 
     for mail in will_be_deleted:
         try:
-            gdrive_service.remove_permission_from_gdrive_folder(folder_link, mail)
+            gdrive_service.remove_permission_from_gdrive_folder(folder_link = folder_link, mail = mail)
             print(f"deleted: {will_be_deleted}")
         except Exception as e:
             print(f"An error occurred when deleting {mail}: {e}")
