b707a01dabf7bba94c15ca2cb22d16d116812aca
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-27 15:48:21 +0300

    Exclude admin email from permission checks in check_list_of_mails function

--- app/services/drive_service.py
+++ app/services/drive_service.py
@@ -1,9 +1,12 @@
 from app.services import gdrive_service
+import os
 
 def check_list_of_mails(folder_link: str, page_mails: list):
     will_be_added = []
     will_be_deleted = []
     existing_mails = []
+
+    ADMIN_MAIL = os.getenv('ADMIN_MAIL')
 
     try:
         exising_permissions = gdrive_service.check_drive_folder_permission(folder_link)
@@ -17,7 +20,7 @@ def check_list_of_mails(folder_link: str, page_mails: list):
                 existing_mails.append(permission['emailAddress'])
 
     for mail in page_mails:
-        if mail not in existing_mails:
+        if mail not in existing_mails and mail != ADMIN_MAIL:
             try:
                 # gdrive_service.add_permission_to_gdrive_folder(folder_link, mail)
                 will_be_added.append(mail)
@@ -25,7 +28,7 @@ def check_list_of_mails(folder_link: str, page_mails: list):
                 print(f"An error occurred when adding {mail}: {e}")
     
     for mail in existing_mails:
-        if mail not in page_mails:
+        if mail not in page_mails and mail != ADMIN_MAIL:
             try:
                 # gdrive_service.remove_permission_from_gdrive_folder(folder_link, mail)
                 will_be_deleted.append(mail)
