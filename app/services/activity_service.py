178294a4360acf6e4e9cefbc4c11522ac1a70fd8
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 20:32:04 +0300

    Refactor activity reporting to use activity repository for manager updates and improve user identification

--- app/services/activity_service.py
+++ app/services/activity_service.py
@@ -3,7 +3,7 @@ import requests
 from app.initializers import env
 from app.models import Manager
 from app.services import send_service
-from app.services import manager_service
+from app.services import activity_repo
 from datetime import datetime
 
 def check_user_activity(username):
@@ -15,20 +15,21 @@ def check_user_activity(username):
     except requests.exceptions.RequestException as e:
         print(f"Failed check_project_activity: {e}")
 
-def check_project_activity(usernames, manager):
+def check_and_report_project_activity(usernames_and_fullnames, manager_json):
     active_users = []
     inactive_users = []
 
-    for username in usernames:
+    for username, fullname in usernames_and_fullnames.items():
         activity = check_user_activity(username)
         if activity:
-            active_users.append(username)
+            active_users.append(fullname)
         else:
-            inactive_users.append(username)
+            inactive_users.append(fullname)
 
-    send_service.send_to_manager_daily_checkup(active_users, inactive_users, manager)
+    send_service.send_to_manager_daily_checkup(active_users, inactive_users, manager_json)
 
-    # update last_reported for manager
-    manager.last_reported = datetime.now()
-    manager_service.update_manager(manager)
+    # get activity and update last_reported
+    activity_obj = activity_repo.get_activity_by_manager_ID(manager_json['number'])
+    activity_obj.last_reported = datetime.now()
+    activity_repo.update_activity(activity_obj)
 
