72ed526c47f5d996364c1f65d1d37a4ebb697f21
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 20:09:58 +0300

    Implement project activity check function and update manager reporting

--- app/services/activity.py
+++ app/services/activity.py
@@ -1,19 +1,10 @@
 import os
 import requests
 from app.initializers import env
-
-# def check_users_activity(list_of_users):
-#     try:
-#         list_of_usernames = []
-#         for name, username in list_of_users:
-#             list_of_usernames.append(username)
-#         ACTIVITY_MS = os.getenv('ACTIVITY_MS')
-#         payload = {'list_of_usernames': list_of_usernames}
-#         response = requests.post(f"{ACTIVITY_MS}", json=payload)
-#         response.raise_for_status()
-#         return response.json()
-#     except requests.exceptions.RequestException as e:
-#         print(f"Failed check_project_activity: {e}")
+from app.models import Manager
+from app.services import send_service
+from app.services import manager_service
+from datetime import datetime
 
 def check_user_activity(username):
     try:
@@ -23,3 +14,21 @@ def check_user_activity(username):
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Failed check_project_activity: {e}")
+
+def check_project_activity(usernames, manager):
+    active_users = []
+    inactive_users = []
+
+    for username in usernames:
+        activity = check_user_activity(username)
+        if activity:
+            active_users.append(username)
+        else:
+            inactive_users.append(username)
+
+    send_service.send_to_manager_daily_checkup(active_users, inactive_users, manager)
+
+    # update last_reported for manager
+    manager.last_reported = datetime.now()
+    manager_service.update_manager(manager)
+
