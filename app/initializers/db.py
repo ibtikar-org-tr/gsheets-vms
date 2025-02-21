0e496d4c0ac54bc12ebfb8f7249fb9c5a17fef0e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 22:55:03 +0300

    Add database initializer and session management in db.py

---
+++ app/initializers/db.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, create_engine, Session
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+engine = create_engine(DATABASE_URL)
+
+def get_session():
+    return Session(engine)
+
