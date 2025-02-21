6d67be000efb570d8b240936f04108edf8f23270
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 23:00:43 +0300

    Add error handling to check_and_report_project_activity for improved robustness

--- app/services/activity_service.py
+++ app/services/activity_service.py
@@ -17,18 +17,20 @@ def check_user_activity(username):
 def check_and_report_project_activity(usernames_and_fullnames, manager_json):
     active_users = []
     inactive_users = []
+    try:
+        for username, fullname in usernames_and_fullnames.items():
+            activity = check_user_activity(username)
+            if activity:
+                active_users.append(fullname)
+            else:
+                inactive_users.append(fullname)
 
-    for username, fullname in usernames_and_fullnames.items():
-        activity = check_user_activity(username)
-        if activity:
-            active_users.append(fullname)
-        else:
-            inactive_users.append(fullname)
+        send_service.send_to_manager_daily_checkup(active_users, inactive_users, manager_json)
 
-    send_service.send_to_manager_daily_checkup(active_users, inactive_users, manager_json)
-
-    # get activity and update last_reported
-    activity_obj = activity_repo.get_activity_by_manager_ID(manager_json['number'])
-    activity_obj.last_reported = datetime.now()
-    activity_repo.update_activity(activity_obj)
+        # get activity and update last_reported
+        activity_obj = activity_repo.get_activity_by_manager_ID(manager_json['number'])
+        activity_obj.last_reported = datetime.now()
+        activity_repo.update_activity(activity_obj)
+    except Exception as e:
+        print(f"Failed check_and_report_project_activity: {e}")
 
