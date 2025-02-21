1db9a40b72271c343b33725980ae269b1c0bc3d2
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 20:17:54 +0300

    Add Activity model to track manager activities with timestamps and identifiers

--- app/models/activity_model.py
+++ app/models/activity_model.py
@@ -3,10 +3,11 @@ from typing import Optional
 from datetime import datetime
 import uuid
 
-class Manager(SQLModel, table=True): # Daily checkup for the manager
+class Activity(SQLModel, table=True): # Daily checkup for the manager
     id: Optional[str] = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     created_at: datetime
     updated_at: datetime
     last_reported: Optional[datetime] = None
     managerName: str
     managerID: str
+    projectName: str
